dd401a875d938065cf50a25fc402e8dcc02d763d
litao <609637057@qq.com>
2014-09-24 10:26:45 +0800

    Initial commit

---
+++ app/src/main/java/com/litao/android/minimalform/MainActivity.java
@@ -0,0 +1,119 @@
+package com.litao.android.minimalform;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.litao.android.lib.MinimalFormLayout;
+import com.litao.android.lib.SubmitListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends Activity implements SubmitListener {
+
+    private TextView mTextViewTest;
+    private MinimalFormLayout mForm;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnClick();
+
+        mForm=(MinimalFormLayout)findViewById(R.id.form);
+        mForm.build(creatData(),creatInputTypes(),creatRegular(),creatErrorMsg());
+        mForm.setOnSubmitListener(this);
+        mForm.setSuccessMsg("Thank you! We'll be in touch.");
+
+    }
+
+    @Override
+    public void onSubmit(List<String> formdata) {
+
+        String s="";
+        for (int i = 0; i < formdata.size(); i++) {
+            s+=i+"."+formdata.get(i)+"\n\n";
+        }
+        mTextViewTest.setText(s);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void btnClick(){
+        findViewById(R.id.previousBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mForm.previous();
+
+            }
+        });
+        findViewById(R.id.nextBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mForm.next();
+            }
+        });
+        findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mForm.reset();
+                mTextViewTest.setText("");
+            }
+        });
+        mTextViewTest=(TextView)findViewById(R.id.testview);
+    }
+
+    private List<String> creatData(){
+        List<String> s=new ArrayList<String>();
+        s.add("What's your username?");
+        s.add("What's your password?");
+        s.add("What's your phone number?");
+        s.add("What's your email address?");
+        return  s;
+    }
+    private List<Integer> creatInputTypes(){
+        List<Integer> s=new ArrayList<Integer>();
+        s.add(InputType.TYPE_CLASS_TEXT);
+        s.add(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        s.add(InputType.TYPE_CLASS_PHONE);
+        s.add(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        return  s;
+    }
+    private List<String> creatErrorMsg(){
+        List<String> s=new ArrayList<String>();
+        s.add("Please enter a valid username");
+        s.add("Please enter a valid password");
+        s.add("Please enter a valid phone number");
+        s.add("Please enter a valid email address");
+        return  s;
+    }
+    private List<String> creatRegular (){
+        List<String> s=new ArrayList<String>();
+        s.add("^[\\u4E00-\\u9FA5A-Za-z][\\u4E00-\\u9FA5A-Za-z0-9]+$");
+        s.add("^[0-9a-zA-Z]\\w{6,18}$");
+        s.add(null);
+        s.add("^\\w+[\\w-\\.]*\\@\\w+((-\\w+)|(\\w*))\\.[a-z]{2,3}$");
+        return  s;
+    }
+
+
+}
---
+++ library/src/main/java/com/litao/android/lib/MinimalFormLayout.java
@@ -0,0 +1,310 @@
+package com.litao.android.lib;
+/*
+ * Copyright 2014 gitonway
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.ValueAnimator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class MinimalFormLayout extends RelativeLayout implements TextWatcher ,Animator.AnimatorListener{
+
+    private static final long ANIMATION_DURATION = 500;
+    private static final int LABLE_SUCCESS_ID = android.R.id.text1;
+
+    private EditText mEditText;
+    private TextView mLabelTitle;
+    private TextView mTextViewSuccess;
+    private TextView mTextPage;
+    private TextView mTextValidator;
+    private ProgressBar mProgressBar;
+    private ImageButton mImageButton;
+
+    private int currentStep = 0;
+    private int progressnumber = 0;
+    private int currentnumber = 0;
+    private int datasize;
+
+    private List<String> titles = null;
+    private List<Integer> inputTypes = null;
+    private List<String> regular = null;
+    private List<String> errorMsg = null;
+    private String[] contents;
+
+    private SubmitListener mSubmitListener;
+
+    public MinimalFormLayout(Context context) {
+        this(context, null);
+    }
+
+    public MinimalFormLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MinimalFormLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        mTextViewSuccess = new TextView(context);
+        RelativeLayout.LayoutParams successParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        successParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        mTextViewSuccess.setId(LABLE_SUCCESS_ID);
+        addView(mTextViewSuccess, successParams);
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if (child instanceof EditText) {
+            mEditText = (EditText) child;
+            mEditText.addTextChangedListener(this);
+        } else if (child instanceof ProgressBar) {
+            mProgressBar = (ProgressBar) child;
+        } else if (child instanceof ImageButton) {
+            mImageButton = (ImageButton) child;
+            mImageButton.setVisibility(INVISIBLE);
+            mImageButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    next();
+                }
+            });
+
+        }
+        if (child.getTag() != null && child.getTag().equals("page")) {
+            mTextPage = (TextView) child;
+        } else if (child.getTag() != null && child.getTag().equals("title")) {
+            mLabelTitle = (TextView) child;
+            mTextViewSuccess.setTextColor(mLabelTitle.getTextColors());
+            mTextViewSuccess.setTextSize(TypedValue.COMPLEX_UNIT_PX, mLabelTitle.getTextSize());
+        } else if (child.getTag() != null && child.getTag().equals("validator")) {
+            mTextValidator = (TextView) child;
+        }
+
+
+        super.addView(child, index, params);
+    }
+
+    public void build(List<String> titles) {
+        build(titles, null);
+    }
+
+    public void build(List<String> titles, List<Integer> inputTypes) {
+        build(titles, inputTypes, null, null);
+    }
+
+    public void build(List<String> titles, List<Integer> inputTypes, List<String> regular, List<String> errorMsg) {
+        this.titles = titles;
+        mLabelTitle.setText(this.titles.get(0));
+        datasize = titles.size();
+        contents = new String[datasize];
+        progressnumber = 100 / datasize;
+        mTextPage.setText((currentStep + 1) + "/" + datasize);
+        mEditText.setText("");
+        mTextValidator.setText("");
+        if (inputTypes != null) {
+            this.inputTypes = inputTypes;
+            mEditText.setInputType(inputTypes.get(0));
+        }
+        if (regular != null) {
+            this.regular = regular;
+        }
+        if (errorMsg != null) {
+            this.errorMsg = errorMsg;
+        }
+    }
+
+    public void next() {
+        if (regular!=null&&regular.get(currentStep) != null) {
+            Pattern p = Pattern.compile(regular.get(currentStep));
+            if (!p.matcher(mEditText.getText().toString()).matches()) {
+                mTextValidator.setText(errorMsg.get(currentStep));
+                ObjectAnimator.ofFloat(mTextValidator, "translationX", 0, 5f,0,4f,0,3f,2f,0).setDuration(ANIMATION_DURATION).start();
+                return;
+            } else {
+                mTextValidator.setText("");
+            }
+        }
+
+        if (currentStep < titles.size() - 1) {
+            contents[currentStep] = mEditText.getText().toString();
+            resetLayout(titles.get(++currentStep));
+            animNext();
+            currentnumber = currentStep * progressnumber;
+        } else if (currentStep == titles.size() - 1) {
+            contents[currentStep] = mEditText.getText().toString();
+            currentStep++;
+            animNext();
+            currentStep--;
+        }
+    }
+
+    public void previous() {
+        if (currentStep > 0) {
+            contents[currentStep] = mEditText.getText().toString();
+            resetLayout(titles.get(--currentStep));
+            animPrevious();
+            currentnumber = currentStep * progressnumber;
+        }
+    }
+
+    public void setSuccessMsg(String msg){
+        mTextViewSuccess.setText(msg);
+    }
+    public void setOnSubmitListener(SubmitListener submitListener){
+        this.mSubmitListener=submitListener;
+    }
+
+
+    private void success() {
+        if (TextUtils.isEmpty(mTextViewSuccess.getText())) {
+            setSuccessMsg("Thank you! We'll be in touch.");
+        }
+            for (int i = 0; i < this.getChildCount(); i++) {
+                View childView = getChildAt(i);
+                if (childView.getId() != LABLE_SUCCESS_ID) {
+                    childView.setVisibility(INVISIBLE);
+                }
+            }
+            ObjectAnimator.ofFloat(mTextViewSuccess, "alpha", 0, 1).setDuration(ANIMATION_DURATION).start();
+        mSubmitListener.onSubmit(Arrays.asList(contents));
+    }
+
+    private void animNext() {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(currentnumber, currentStep * progressnumber);
+        valueAnimator.setDuration(ANIMATION_DURATION);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int i = (Integer) valueAnimator
+                        .getAnimatedValue();
+                if (i == datasize * progressnumber) {
+                    success();
+                } else {
+                    mProgressBar.setProgress(i);
+                }
+            }
+        });
+        valueAnimator.start();
+    }
+
+    private void animPrevious() {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(currentnumber, currentStep * progressnumber);
+        valueAnimator.setDuration(ANIMATION_DURATION);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mProgressBar.setProgress((Integer) valueAnimator
+                        .getAnimatedValue());
+            }
+        });
+        valueAnimator.start();
+    }
+
+    private void resetLayout(String title) {
+        ObjectAnimator objectAnimator=ObjectAnimator.ofFloat(mLabelTitle, "translationY", 0, -mLabelTitle.getHeight()).setDuration(ANIMATION_DURATION/2);
+        objectAnimator.addListener(this);
+        objectAnimator.start();
+
+        mEditText.setText(contents[currentStep]);
+        mEditText.setInputType(inputTypes.get(currentStep));
+        mTextPage.setText((currentStep + 1) + "/" + datasize);
+        mEditText.setSelection(contents[currentStep] == null ? 0 : contents[currentStep].length());
+    }
+
+    public void reset() {
+        currentStep = 0;
+        currentnumber = 0;
+        build(titles);
+        mProgressBar.setProgress(0);
+        for (int i = 0; i < this.getChildCount(); i++) {
+            View childView = getChildAt(i);
+            if (childView.getId() != LABLE_SUCCESS_ID) {
+                childView.setVisibility(VISIBLE);
+            }
+
+        }
+        mImageButton.setVisibility(GONE);
+    }
+
+
+    private int dipsToPix(float dps) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dps,
+                getResources().getDisplayMetrics());
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        if (!TextUtils.isEmpty(charSequence)) {
+            mImageButton.setVisibility(VISIBLE);
+        } else {
+            mImageButton.setVisibility(INVISIBLE);
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        mLabelTitle.setText(titles.get(currentStep));
+        ObjectAnimator.ofFloat(mLabelTitle, "translationY",  mLabelTitle.getHeight(),0).setDuration(ANIMATION_DURATION/2).start();
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
